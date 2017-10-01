83d1a965ed8ad67f8e22a45338b31dda613db37e
Ceri James <jamesceri@yahoo.co.uk>
2017-10-01 19:50:07 +0100

    Add wiring pi

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
-#include <stdio.h>
+#include <stdio.h>    // Used for printf() statements
+#include <wiringPi.h> // Include WiringPi library!
 
-int
-main(int argc, char* argv[])
+// Pin number declarations. We're using the Broadcom chip pin numbers.
+const int ledPin = 23; // Regular LED - Broadcom pin 23, P1 pin 16
+const int pirPin = 17; // Active-high PIR - Broadcom pin 17, P1 pin 11
+
+int main(void)
 {
+    // Setup stuff:
+    wiringPiSetupGpio();            // Initialize wiringPi -- using Broadcom pin numbers
+
+    pinMode(ledPin, OUTPUT);        // Set regular LED as output
+    digitalWrite(ledPin, HIGH);     // Turn LED ON
+
     printf("Hello world\n");
+
     return 0;
 }
